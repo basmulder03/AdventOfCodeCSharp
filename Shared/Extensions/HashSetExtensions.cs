fe443f8d445f12d0b0412b17a659fee426658244
Bas Mulder <Bas.Mulder@rr-wfm.com>
2023-11-03 22:12:28 +0100

    Add updates of the files in the shared folder for 2015-7

---
+++ Shared/Extensions/HashSetExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Extensions
+{
+    public static class HashSetExtensions
+    {
+        public static T GetOrAdd<T>(this HashSet<T> set, T value) where T : IEquatable<T>
+        {
+            if (set.Add(value)) return value;
+            else return set.First(setValue => setValue.Equals(value));
+        }
+    }
+}
---
+++ Shared/Helpers/HashMap.cs
@@ -0,0 +1,47 @@
+﻿namespace Shared.Helpers
+{
+    public class HashMap<T, K> where T : IEquatable<T>
+    {
+        private readonly Dictionary<T, K> _dictionary;
+
+        public HashMap()
+        {
+            _dictionary = new Dictionary<T, K>();
+        }
+
+        public T[] Keys()
+        {
+            return _dictionary.Keys.ToArray();
+        }
+
+        public K[] Values()
+        {
+            return _dictionary.Values.ToArray();
+        }
+
+        public void Add(T key, K value)
+        {
+            _dictionary.Add(key, value);
+        }
+
+        public K Get(T key)
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return _dictionary.GetValueOrDefault(key);
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        public K GetOrAdd(T key, K value)
+        {
+            if (_dictionary.ContainsKey(key))
+            {
+                return _dictionary[key];
+            }
+            else
+            {
+                _dictionary[key] = value;
+                return value;
+            }
+        }
+    }
+}
