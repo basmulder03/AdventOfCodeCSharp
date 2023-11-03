badbbbb888dfbddc992e9aae17c9def85ea7569b
Bas Mulder <Bas.Mulder@rr-wfm.com>
2023-11-03 22:11:49 +0100

    Add all other days 2015

---
+++ 2015/days/Day14.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode;
+
+namespace _2015.days
+{
+    internal class Day14 : IDaySolution
+    {
+
+        public void SolutionPart1(string data)
+        {
+            Console.WriteLine("Not yet implemented");
+        }
+
+        public void SolutionPart2(string data)
+        {
+            Console.WriteLine("Not yet implemented");
+        }
+    }
+}
